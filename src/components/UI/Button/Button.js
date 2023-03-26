100869f462a0459a0f8b14b297d0f60709a401cd
Mamuni98 <nabanitasahoo468@gmail.com>
2023-03-26 11:37:49 +0530

    Dynamic_Styling_(Task-1)

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Button.css";
+
+const Button = (props) => {
+  const classes = "button " + props.className;
+  return (
+    <button className={classes} type={props.type} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
